1066bfa28570ceb4449bf3a852de6ad91476fb20
Cicero Lino <cicerolinoeneto@gmail.com>
2024-04-18 01:37:56 -0300

    feat: broken feature, tentative getAll route and swagger implementation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DataController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/products', [DataController::class, 'getAllProducts']);
+Route::get('/sales', [DataController::class, 'getAllSales']);
+Route::get('/customers', [DataController::class, 'getAllCustomers']);
