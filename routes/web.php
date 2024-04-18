c3d035c4aa5d293d2981bcb19b4e4711184618eb
Cicero Lino <cicerolinoeneto@gmail.com>
2024-04-18 02:08:58 -0300

    wiṕ

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use App\Http\Controllers\DataController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/products', [DataController::class, 'getAllProducts']);
-Route::get('/sales', [DataController::class, 'getAllSales']);
-Route::get('/customers', [DataController::class, 'getAllCustomers']);
+Route::get('/products', function () {
+    return Inertia::render('Products/Index');
+})->name('products.index');
+
+Route::get('/sales', function () {
+    return Inertia::render('Sales/Index');
+})->name('sales.index');
+
+Route::get('/customers', function () {
+    return Inertia::render('Customers/Index');
+})->name('customers.index');
